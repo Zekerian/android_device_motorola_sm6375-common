1677ab78acc908b82b7a3ed98011c4d2e7753521
jasiek1232 <130497712+jasiek1232@users.noreply.github.com>
2025-03-31 20:54:41 +0200

    Reapply "sm6375-common: switch to common QCOM AIDL bootctrl HAL"
    This reverts commit bfc6d8466a031fcf7d2b67593d38e0bfd4817e8d.

--- common.mk
+++ common.mk
@@ -68,12 +68,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries-qti.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-qti.txt
 
 # Boot control
+PRODUCT_SOONG_NAMESPACES += hardware/qcom-caf/bootctrl
+
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-qti \
-    android.hardware.boot@1.2-impl-qti.recovery \
-    android.hardware.boot@1.2-service \
-    bootctrl.holi \
-    bootctrl.holi.recovery
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
